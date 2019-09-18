e42a4e8998af8afa3476596bbfcbe1d95ecb02aa
Eirik Dahlen <37441744+eirikdahlen@users.noreply.github.com>
2019-09-18 16:25:22 +0200

    #3 chore: setup linter (#27)
    * #3 chore: add eslint and prettier config
    
    * #3 chore: bypass linting error with ignore
    
    * #3 chore: change max-len on lines in linter

--- src/VideoComponents/js/depth.js
+++ src/VideoComponents/js/depth.js
@@ -1,12 +1,12 @@
 function depth_init(context_depth) {
-	// Basic formatting
-	const color_base = "#FFFFFF"
-	context_depth.strokeStyle = color_base;
-	context_depth.fillStyle = color_base;
-	context_depth.textAlign = "right";
-	context_depth.textBaseline = "middle";
+  // Basic formatting
+  const color_base = '#FFFFFF';
+  context_depth.strokeStyle = color_base;
+  context_depth.fillStyle = color_base;
+  context_depth.textAlign = 'right';
+  context_depth.textBaseline = 'middle';
 
-	draw_indicator_depth(context_depth);
+  draw_indicator_depth(context_depth);
 }
 
 // Dummy animation
@@ -14,52 +14,52 @@ var offset_depth = 0;
 var offset_depth_mult = -1;
 
 function drawDepth(context_depth) {
-	const num_space = 50;  // Spacing between the numerical labels
+  const num_space = 50; // Spacing between the numerical labels
 
-	// Clear the canvas every frame (except the rightmost triangle, which is static)
-	context_depth.clearRect(0, 0, 47, 500);
+  // Clear the canvas every frame (except the rightmost triangle, which is static)
+  context_depth.clearRect(0, 0, 47, 500);
 
-	// Add 200 labels (the ROV never descends more than 200 meters)
-	for (var i = 0; i < 201; i++) {
-		var y_position = i * num_space + 250 + offset_depth;  // Calculate y position of each label
+  // Add 200 labels (the ROV never descends more than 200 meters)
+  for (var i = 0; i < 201; i++) {
+    var y_position = i * num_space + 250 + offset_depth; // Calculate y position of each label
 
-		// Enlarge every 5th label
-		if (i % 5 === 0) {
-			context_depth.font = "15px Arial";
-		} else {
-			context_depth.font = "11px Arial";
-		}
+    // Enlarge every 5th label
+    if (i % 5 === 0) {
+      context_depth.font = '15px Arial';
+    } else {
+      context_depth.font = '11px Arial';
+    }
 
-		// Draw the actual label
-		context_depth.fillText(i.toString(), 30, y_position);
+    // Draw the actual label
+    context_depth.fillText(i.toString(), 30, y_position);
 
-		// Draw indicator lines next to every label
-		context_depth.beginPath();
-		context_depth.moveTo(37, y_position);
-		context_depth.lineTo(47, y_position);
-		context_depth.stroke();
-	}
+    // Draw indicator lines next to every label
+    context_depth.beginPath();
+    context_depth.moveTo(37, y_position);
+    context_depth.lineTo(47, y_position);
+    context_depth.stroke();
+  }
 
-	// Dummy animation
-	if (y_position <= 250) {
-		offset_depth_mult = 1;
-	} else if (y_position >= 201 * num_space + 250) {
-		offset_depth_mult = -1;
-	}
+  // Dummy animation
+  if (y_position <= 250) {
+    offset_depth_mult = 1;
+  } else if (y_position >= 201 * num_space + 250) {
+    offset_depth_mult = -1;
+  }
 
-	// Dummy animation
-	offset_depth += 5 * offset_depth_mult;
+  // Dummy animation
+  offset_depth += 5 * offset_depth_mult;
 }
 
 function draw_indicator_depth(context_depth) {
-	// Draw the static indicator triangle
-	context_depth.beginPath();
-	context_depth.moveTo(50, 250);
-	context_depth.lineTo(60, 253);
-	context_depth.lineTo(60, 247);
-	context_depth.lineTo(50, 250);
-	context_depth.stroke();
-	context_depth.fill();
+  // Draw the static indicator triangle
+  context_depth.beginPath();
+  context_depth.moveTo(50, 250);
+  context_depth.lineTo(60, 253);
+  context_depth.lineTo(60, 247);
+  context_depth.lineTo(50, 250);
+  context_depth.stroke();
+  context_depth.fill();
 }
 
-export {depth_init, drawDepth};
+export { depth_init, drawDepth };
