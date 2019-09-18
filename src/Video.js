f3bdb6c40f4e8da7f7d9e9a543de7628c7ba3c6a
Eirik Dahlen <dahleneirik@gmail.com>
2019-09-18 14:28:40 +0200

    #3 chore: add eslint and prettier config

--- src/Video.js
+++ src/Video.js
@@ -1,53 +1,64 @@
 // The App for the VideoWindow. This is where every video-component should go.
 
-import React from "react";
-import drawBias from "./VideoComponents/js/bias.js";
-import {drawHeading, heading_init} from "./VideoComponents/js/heading.js";
-import {drawDepth, depth_init} from "./VideoComponents/js/depth.js";
-import "./VideoComponents/css/overlay.css"
+import React from 'react';
+import drawBias from './VideoComponents/js/bias.js';
+import { drawHeading, heading_init } from './VideoComponents/js/heading.js';
+import { drawDepth, depth_init } from './VideoComponents/js/depth.js';
+import './VideoComponents/css/overlay.css';
 
 class VideoApp extends React.Component {
+  componentDidMount() {
+    // Get the bias canvas
+    let canvas_bias = this.refs.canvasBias;
+    let context_bias = canvas_bias.getContext('2d');
 
-	componentDidMount() {
-		// Get the bias canvas
-		let canvas_bias = this.refs.canvasBias;
-		let context_bias = canvas_bias.getContext("2d");
+    // Get the heading canvas
+    let canvas_heading = this.refs.canvasHeading;
+    let context_heading = canvas_heading.getContext('2d');
 
-		// Get the heading canvas
-		let canvas_heading = this.refs.canvasHeading;
-		let context_heading = canvas_heading.getContext("2d");
+    // Get the depth canvas
+    let canvas_depth = this.refs.canvasDepth;
+    let context_depth = canvas_depth.getContext('2d');
 
-		// Get the depth canvas
-		let canvas_depth = this.refs.canvasDepth;
-		let context_depth = canvas_depth.getContext("2d");
+    // Initialize the heading and depth widgets (color, indicators, ...)
+    heading_init(context_heading);
+    depth_init(context_depth);
 
-		// Initialize the heading and depth widgets (color, indicators, ...)
-		heading_init(context_heading);
-		depth_init(context_depth);
+    // Function for redrawing all canvases in one interval
+    function redraw_overlay() {
+      drawBias(context_bias);
+      drawHeading(context_heading);
+      drawDepth(context_depth);
+    }
 
-		// Function for redrawing all canvases in one interval
-		function redraw_overlay() {
-			drawBias(context_bias);
-			drawHeading(context_heading);
-			drawDepth(context_depth);
-		}
+    // Redraw the canvases continuously
+    setInterval(redraw_overlay, 30);
+  }
 
-		// Redraw the canvases continuously
-		setInterval(redraw_overlay, 30);
-	}
-
-	render() {
-		return (
-			<div className="VideoApp">
-			  <canvas id="canvasBias" ref="canvasBias" width={500} height={500}>
-			  </canvas>
-			  <canvas id="canvasHeading" ref="canvasHeading" width={800} height={75}>
-			  </canvas>
-			  <canvas id="canvasDepth" ref="canvasDepth" width={75} height={500}>
-			  </canvas>
-			</div>
-		)
-	}
+  render() {
+    return (
+      <div className="VideoApp">
+        <canvas
+          id="canvasBias"
+          ref="canvasBias"
+          width={500}
+          height={500}
+        ></canvas>
+        <canvas
+          id="canvasHeading"
+          ref="canvasHeading"
+          width={800}
+          height={75}
+        ></canvas>
+        <canvas
+          id="canvasDepth"
+          ref="canvasDepth"
+          width={75}
+          height={500}
+        ></canvas>
+      </div>
+    );
+  }
 }
 
 /*function VideoApp() {
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 //index.js is the file that is loaded when the app is loaded.
 //Its component - ViewManager - handles which of Control.js and Video.js to load.
 
-import React from "react";
-import ReactDOM from "react-dom";
-import ViewManager from "./ViewManager";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ViewManager from './ViewManager';
 
-ReactDOM.render(<ViewManager />, document.getElementById("root"));
+ReactDOM.render(<ViewManager />, document.getElementById('root'));
