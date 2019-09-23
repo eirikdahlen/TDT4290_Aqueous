d84451ec294e37fe9b19bf766b2196c4364756f1
Sindre Aarnes Aubert <sindreaubert@gmail.com>
2019-09-23 15:47:54 +0200

    #14 feature: setup for sending controls to ROV

--- public/IPC.js
+++ public/IPC.js
@@ -3,13 +3,21 @@
 // main -> view
 // view -> main -> view
 const { getConnectedClient } = require('./TCP/TCPClient');
+const { ipcMain } = require('electron');
+const { handleClick, setUpOrDown } = require('./xbox/clickHandler');
 
 let client;
 
 // Function for setting up listeners between the main process (electron.js) and the renderer process (Components etc.)
 function setIPCListeners() {
   //For testing purposes: The client should in reality be created right after the simulator is opened
-  client = getConnectedClient();
+  //client = getConnectedClient();
+  ipcMain.on('xbox-change', (event, { button, value }) => {
+    handleClick({ button, value });
+  });
+  ipcMain.on('xbox-up-down', (event, { button, down }) => {
+    setUpOrDown({ button, down });
+  });
 }
 
 module.exports = { setIPCListeners };
