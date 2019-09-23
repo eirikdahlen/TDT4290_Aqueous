b403cabd691c48930e91a0124233fd6e2cc9849d
al3xknutsen <al3xknutsen@gmail.com>
2019-09-23 13:40:27 +0200

    feature: add support for dual-monitor setup

--- public/electron.js
+++ public/electron.js
@@ -14,18 +14,25 @@ const { setIPCListeners } = require('./IPC');
 let controlWindow;
 let videoWindow;
 
-let width;
-let height;
+let widthControlWindow;
+let heightControlWindow;
+let widthVideoWindow;
+let heightVideoWindow;
+
+let xControlWindow;
+let yControlWindow;
+let xVideoWindow;
+let yVideoWindow;
 
 //Function for creating the two windows - controls and video
 function createWindows() {
   // Creates the two windows with positioning, width and height fitting the screen
   videoWindow = new BrowserWindow({
     title: 'Video feed',
-    width: width / 2,
-    height: height,
-    x: 0,
-    y: 0,
+    width: widthVideoWindow,
+    height: heightVideoWindow,
+    x: xVideoWindow,
+    y: yVideoWindow,
     webPreferences: {
       nodeIntegration: true,
     },
@@ -40,10 +47,10 @@ function createWindows() {
 
   controlWindow = new BrowserWindow({
     title: 'Controls',
-    width: width / 2,
-    height: height,
-    x: width - width / 2,
-    y: 0,
+    width: widthControlWindow,
+    height: heightControlWindow,
+    x: xControlWindow,
+    y: yControlWindow,
     webPreferences: {
       nodeIntegration: true,
     },
@@ -79,13 +86,45 @@ function createWindows() {
 
 // Sets the width and height of screen - for positioning the created windows according to screen size
 function setWidthAndHeight() {
-  const display = electron.screen.getPrimaryDisplay();
-  width = display.bounds.width;
-  height = display.bounds.height;
+  // Get the primary screen, as well as a complete list of all available screens
+  const mainDisplay = electron.screen.getPrimaryDisplay();
+  const allDisplays = electron.screen.getAllDisplays();
+
+  // Find which screen is the external one
+  let externalDisplay = allDisplays.find(display => {
+    return display.bounds.x !== 0 || display.bounds.y !== 0;
+  });
+
+  // If an external screen is detected:
+  if (externalDisplay) {
+    // Set the size if the video window to the full size of the main screen
+    heightVideoWindow = mainDisplay.bounds.height;
+    widthVideoWindow = mainDisplay.bounds.width;
+
+    // Set the size of the control window to the full size of the external screen
+    heightControlWindow = externalDisplay.bounds.height;
+    widthControlWindow = externalDisplay.bounds.width;
+
+    // Position the windows on their respective screens
+    xVideoWindow = mainDisplay.bounds.x;
+    yVideoWindow = mainDisplay.bounds.y;
+    xControlWindow = externalDisplay.bounds.x;
+    yControlWindow = externalDisplay.bounds.y;
+
+    // If an external screen is *not* detected:
+  } else {
+    // Set the widths of the window to half the width of the screen
+    heightVideoWindow = heightControlWindow = mainDisplay.bounds.height;
+    widthVideoWindow = widthControlWindow = mainDisplay.bounds.width / 2;
+
+    // Position the video window to the left and the control window to the right
+    xVideoWindow = mainDisplay.bounds.x;
+    yVideoWindow = yControlWindow = mainDisplay.bounds.y;
+    xControlWindow = mainDisplay.bounds.width - mainDisplay.bounds.width / 2;
+  }
 }
 
 // Functions that are run when the app is ready
-
 app.on('ready', () => {
   // Sets menu for controlVindow (from public/menuTemplate.js) and removes menu from videoWindow
   const controlMenu = Menu.buildFromTemplate(menuTemplate);
