15b80cd4ba57cfdf693317ac36d3efeeaafda70e
Henrik Grønbech <henrikgronbech@gmail.com>
2019-09-23 12:50:56 +0200

    # 11: Improve sendData() arguments

--- public/TCP/TCPClient.js
+++ public/TCP/TCPClient.js
@@ -17,16 +17,31 @@ function getConnectedClient() {
   // Handles receiving data
   client.on("data", function(data) {
     data = decodeData(data);
-    console.log(`[${Date.now()}] Recieved data from server:`);
+    console.log(`\n[${Date.now()}] Recieved data from server:`);
     console.log(data);
+    // TODO: This function should probably update global state somehow
   });
   return client;
 }
 
 function sendData(client, data) {
+  /**
+   * data should be a object with these fields: 
+   * {'surge': number, 
+   *  'sway': number, 
+   *  'heave': number, 
+   *  'roll': number, 
+   *  'pitch': number, 
+   *  'yaw': number, 
+   *  'autodepth': bool, 
+   *  'autoheading': bool
+   * }
+   */
   // Add function (encodeData) for formatting data to be sent here
-  data = encodeData(data);
-  client.write(data);
+  buf = encodeData(data);
+  console.log(`\n[${Date.now()}] Sending byte array with data:`);
+  console.log(data);
+  client.write(buf);
 }
 
 function sendDummyData(client) {
@@ -34,10 +49,19 @@ function sendDummyData(client) {
   // Force surge, Force sway, Force heave, ...
   // Commanded Moment in roll (always 0), Commanded Moment in pitch (always 0),
   // Commanded Moment in yaw, Flag autodepth (0/1 off/on), Flag autoheading (0/1 off/on)
-  const doublesArray = new Float64Array([0.0, 0.0, 1.5, 0.0, 0.0, 1, 0, 0]);
-  const buf = Buffer.from(doublesArray.buffer);
-  console.log(`Sending byte array ${doublesArray}`);
-  sendData(client, buf);
+  // const doublesArray = new Float64Array([0.0, 0.0, 1.5, 0.0, 0.0, 1, 0, 0]);
+  // const buf = Buffer.from(doublesArray.buffer);
+  // console.log(`Sending byte array ${doublesArray}`);
+  sendData(client, {
+    'surge': 0,
+    'sway': 0,
+    'heave': 100,
+    'roll': 0,
+    'pitch': 0,
+    'yaw': 50,
+    'autodepth': false,
+    'autoheading': false
+  });
 }
 
 function sendDummyDataContinuously(client, interval) {
