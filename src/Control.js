75b7e81dab0e6fb50b6d4cae6c722bdf41f40dd0
hsyver <hsyverinsen@hotmail.com>
2019-09-25 21:53:44 +0200

    #39 #41 feature: Make lock component set toROV-values

--- src/Control.js
+++ src/Control.js
@@ -10,6 +10,7 @@ import Lock from './ControlComponents/js/Lock';
 import './ControlComponents/css/control.css';
 import GamepadWrapper from './ControlComponents/GamepadWrapper';
 
+<<<<<<< HEAD
 const { remote } = window.require('electron');
 
 function ControlApp() {
@@ -20,6 +21,13 @@ function ControlApp() {
       sensorUpdate(remote.getGlobal('fromROV'));
     });
   }, []);
+=======
+const remote = window.require('electron').remote;
+
+function ControlApp() {
+  const dummyValues = [2.11, 1.12, 20.89, 0.01, 0.0, 234.59];
+  let toROV = remote.getGlobal('toROV');
+>>>>>>> #39 #41 feature: Make lock component set toROV-values
 
   return (
     <div className="ControlApp">
@@ -31,13 +39,32 @@ function ControlApp() {
       <div className="controlFlex">
         <ModeMenu mode="Manual" /> {/*Start in mode Manual*/}
         <div className="autoFlex">
-          <Lock title="AutoDepth" min={0} max={200} step={0.1}/>
-          <Lock title="AutoHeading" min={0} max={360} step={0.5} loop={true}/>
+          <Lock
+            title="AutoDepth"
+            active={toROV.autodepth}
+            value={toROV.heave}
+            min={0}
+            max={200}
+            step={0.1}
+          />
+          <Lock
+            title="AutoHeading"
+            active={toROV.autoheading}
+            value={toROV.yaw}
+            min={0}
+            max={360}
+            step={0.5}
+            loop={true}
+          />
         </div>
       </div>
+<<<<<<< HEAD
       <Values sensorValues={sensorValues} />
       {/*Should be values={props.values} 
       where props.values is passed from ViewManager?*/}
+=======
+      <Values values={dummyValues} />
+>>>>>>> #39 #41 feature: Make lock component set toROV-values
       <GamepadWrapper />
     </div>
   );
