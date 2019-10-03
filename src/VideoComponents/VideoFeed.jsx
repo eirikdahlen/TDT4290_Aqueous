27bbc965d3817dc813c7a61a3ea6862a51ad9da8
Sindre Aarnes Aubert <sindreaubert@gmail.com>
2019-10-03 16:06:28 +0200

    #61 style: finishing styling and a little refactoring

--- src/VideoComponents/VideoFeed.jsx
+++ src/VideoComponents/VideoFeed.jsx
@@ -6,13 +6,14 @@ class VideoFeed extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      videoFeeds: {},
-      deviceId: '',
-      deviceLabel: '',
+      devices: {},
+      currentID: '',
+      currentLabel: '',
       width: 0,
       height: 0,
     };
   }
+
   // Function for updating size - is called when window is resized to make webcam fit window properly
   updateDimensions = () => {
     this.setState({
@@ -21,61 +22,67 @@ class VideoFeed extends Component {
     });
   };
 
+  // Switches feed when a feed-button is clicked
   switchFeed = event => {
-    const label = event.target.innerHTML;
-    const clickedFeed = this.state.videoFeeds[label];
-    this.setState({ deviceId: clickedFeed, deviceLabel: label });
-    var buttons = document.querySelectorAll('.videoButton');
+    const btnClicked = event.target;
+    const clickedLabel = btnClicked.innerHTML;
+    const clickedID = this.state.devices[clickedLabel];
+    this.setState({ currentID: clickedID, currentLabel: clickedLabel });
+    // Adds selected-styling only to the button recently clicked
+    const buttons = document.querySelectorAll('.videoButton');
     buttons.forEach(btn => {
       btn.classList.remove('selectedFeed');
     });
-    event.target.classList.add('selectedFeed');
+    btnClicked.classList.add('selectedFeed');
   };
 
-  setVideoObject = videoFeeds => {
+  // Takes in a list of devices and sets the state to the data needed
+  setVideoObject = devices => {
     let feeds = {};
-    videoFeeds.forEach(feed => {
+    devices.forEach(feed => {
       let { label } = feed;
       label = label.substr(0, label.indexOf('(') - 1);
       feeds[label] = feed.deviceId;
     });
-    this.setState({ videoFeeds: feeds });
+    this.setState({ devices: feeds });
   };
 
-  setDefaultId = () => {
-    let { videoFeeds } = this.state;
-    const keys = Object.keys(videoFeeds);
-    let ID = videoFeeds[keys[0]].deviceId;
+  // Sets Cam Link as default cam to use, but if it is not connected we use the first cam
+  setDefaultID = () => {
+    let { devices } = this.state;
+    const keys = Object.keys(devices);
+    let ID = devices[keys[0]].currentID;
     let label = keys[0];
     keys.forEach(key => {
       if (key.includes('Cam Link')) {
-        ID = videoFeeds[key];
+        ID = devices[key];
         label = key;
       }
     });
-    this.setState({ deviceId: ID, deviceLabel: label });
+    this.setState({ currentID: ID, currentLabel: label });
   };
 
   init = () => {
     navigator.mediaDevices.enumerateDevices().then(devices => {
-      const videoFeeds = devices.filter(feed => {
+      const feeds = devices.filter(feed => {
         return feed.kind === 'videoinput';
       });
-      this.setVideoObject(videoFeeds);
-      this.setDefaultId();
+      this.setVideoObject(feeds);
+      this.setDefaultID();
       this.populateDropdown();
     });
   };
 
+  // Creates buttons inside the dropdown menu based on the devices-state
   populateDropdown = () => {
     const dropdown = document.getElementById('video-dropdown-content');
     dropdown.innerHTML = '';
-    Object.keys(this.state.videoFeeds).forEach(key => {
+    Object.keys(this.state.devices).forEach(key => {
       const btn = document.createElement('button');
       btn.onclick = this.switchFeed;
       btn.innerHTML = key;
       btn.classList.add('videoButton');
-      if (key === this.state.deviceLabel) {
+      if (key === this.state.currentLabel) {
         btn.classList.add('selectedFeed');
       }
       dropdown.appendChild(btn);
@@ -106,7 +113,7 @@ class VideoFeed extends Component {
           <div id="video-dropdown-content"></div>
         </div>
         <Webcam
-          videoConstraints={{ deviceId: this.state.deviceId }}
+          videoConstraints={{ deviceId: this.state.currentID }}
           height={this.state.height}
           width={this.state.width}
         />
