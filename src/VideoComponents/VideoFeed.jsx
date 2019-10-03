0d842e092e62763be5c53a782191c914b9afb908
Sindre Aarnes Aubert <sindreaubert@gmail.com>
2019-10-03 13:40:29 +0200

    #61 feature: functioning video-picker

--- src/VideoComponents/VideoFeed.jsx
+++ src/VideoComponents/VideoFeed.jsx
@@ -1,10 +1,11 @@
 import Webcam from 'react-webcam';
 import React, { Component } from 'react';
+import './css/VideoFeed.css';
 
 class VideoFeed extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = { videoFeeds: {}, deviceId: '', width: 0, height: 0 };
   }
   // Function for updating size - is called when window is resized to make webcam fit window properly
   updateDimensions = () => {
@@ -14,29 +15,47 @@ class VideoFeed extends Component {
     });
   };
 
-  setDeviceId = () => {
+  switchFeed = event => {
+    const label = event.target.innerHTML;
+    const clickedFeed = this.state.videoFeeds[label];
+    this.setState({ deviceId: clickedFeed });
+  };
+
+  setVideoObject = videoFeeds => {
+    let feeds = {};
+    videoFeeds.forEach(feed => {
+      let { label } = feed;
+      label = label.substr(0, label.indexOf('(') - 1);
+      feeds[label] = feed.deviceId;
+    });
+    this.setState({ videoFeeds: feeds });
+  };
+
+  init = () => {
     navigator.mediaDevices.enumerateDevices().then(devices => {
-      const videoFeeds = devices.filter(el => {
-        return el.kind === 'videoinput';
+      const videoFeeds = devices.filter(feed => {
+        return feed.kind === 'videoinput';
       });
-      console.log(videoFeeds);
-      let chosenFeedId = '';
-      if (videoFeeds.length === 1) {
-        chosenFeedId = videoFeeds[0].deviceId;
-      } else {
-        const camLinkFeed = videoFeeds.filter(feed => {
-          return feed.label.indexOf('Cam Link') >= 0;
-        });
-        chosenFeedId = camLinkFeed.deviceId;
-      }
-      this.setState({ deviceId: chosenFeedId });
+      this.setVideoObject(videoFeeds);
+      this.populateDropdown();
+    });
+  };
+
+  populateDropdown = () => {
+    console.log('hei');
+    const dropdown = document.getElementById('video-dropdown-content');
+    Object.keys(this.state.videoFeeds).forEach(key => {
+      const btn = document.createElement('button');
+      btn.onclick = this.switchFeed;
+      btn.innerHTML = key;
+      dropdown.appendChild(btn);
     });
   };
 
   // componentDidMount is built-in function that is called after the inital rendering of the component
   // Adds eventlistener on resizing window, and updates width and height in state accordingly.
   componentDidMount() {
-    this.setDeviceId();
+    this.init();
     this.updateDimensions();
     window.addEventListener('resize', e => {
       e.preventDefault();
@@ -52,6 +71,10 @@ class VideoFeed extends Component {
   render() {
     return (
       <div className="VideoFeed">
+        <div className="dropdown">
+          <button className="dropbtn"></button>
+          <div id="video-dropdown-content"></div>
+        </div>
         <Webcam
           videoConstraints={{ deviceId: this.state.deviceId }}
           height={this.state.height}
