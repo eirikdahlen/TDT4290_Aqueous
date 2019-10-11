482bb0dd5431f18d7e99c60bbbaa9637acd1c547
Sindre Aarnes Aubert <sindreaubert@gmail.com>
2019-10-11 09:38:41 +0200

    #55 refactor: simplify message-sending

--- public/utils/IPC.js
+++ public/utils/IPC.js
@@ -1,14 +1,11 @@
 // Handles interprocess communications:
 // view -> main
 // main -> view
-// view -> main -> view
 const { ipcMain } = require('electron');
 const { handleClick, setUpOrDown } = require('./../controls/mapping');
 
 // Function for setting up listeners between the main process (electron.js) and the renderer process (Components etc.)
 function setIPCListeners() {
-  //For testing purposes: The client should in reality be created right after the simulator is opened
-  //client = getConnectedClient();
   ipcMain.on('button-click', (event, { button, value }) => {
     handleClick({ button, value });
   });
@@ -17,26 +14,15 @@ function setIPCListeners() {
   });
 }
 
-//Sends data to the two renderers/browser windows
-function sendReceivedMessage() {
+//Sends messages to the two renderers/browser windows
+function sendMessage(msg) {
   const { videoWindow, controlWindow } = global;
   try {
-    videoWindow.webContents.send('data-received');
-    controlWindow.webContents.send('data-received');
+    videoWindow.webContents.send(msg);
+    controlWindow.webContents.send(msg);
   } catch (error) {
     console.log('Windows are closed');
   }
 }
 
-//Sends data to the two renderers/browser windows
-function sendSentMessage() {
-  const { videoWindow, controlWindow } = global;
-  try {
-    videoWindow.webContents.send('data-sent');
-    controlWindow.webContents.send('data-sent');
-  } catch (error) {
-    console.log('Windows are closed');
-  }
-}
-
-module.exports = { setIPCListeners, sendReceivedMessage, sendSentMessage };
+module.exports = { setIPCListeners, sendMessage };
