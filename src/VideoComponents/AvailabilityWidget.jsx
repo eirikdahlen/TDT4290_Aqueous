d296d21a9e7329e89299a1ea766a77ff8b82bcf9
hsyver <hsyverinsen@hotmail.com>
2019-10-21 15:40:20 +0200

    #151 feature: add availability widget

---
+++ src/VideoComponents/AvailabilityWidget.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './css/AvailabilityWidget.css';
+
+const textStyle = {
+  color: '#FFFFFF',
+};
+
+export default function AvailabilityWidget({ nfAvailable, dpAvailable }) {
+  return (
+    <div className="AvailabilityWidget">
+      <div className="nfdp">
+        <h3 style={textStyle}>NF</h3>
+        <LightDot available={nfAvailable} />
+      </div>
+      <div className="nfdp">
+        <h3 style={textStyle}>DP</h3>
+        <LightDot available={dpAvailable} />
+      </div>
+    </div>
+  );
+}
+
+function LightDot({ available }) {
+  return (
+    <div>
+      <span
+        className={
+          'availabilityMark ' + (available ? 'availability' : 'noAvailability')
+        }
+      ></span>
+    </div>
+  );
+}
+
+AvailabilityWidget.propTypes = {
+  nfAvailable: PropTypes.bool,
+  dpAvailable: PropTypes.bool,
+};
+
+LightDot.propTypes = {
+  available: PropTypes.bool,
+};
