be17b5f9497d7e85f9b7d91e51fae24ac89b6c49
al3xknutsen <al3xknutsen@gmail.com>
2019-10-28 14:23:18 +0100

    chore: try to sovle merge conflicts

--- public/electron.js
+++ public/electron.js
@@ -6,104 +6,18 @@ if (setupEvents.handleSquirrelEvent()) {
 // electron.js is the main process for electron. It handles windows and communication between windows.
 const electron = require('electron');
 const { app, Menu } = electron;
-
 const isDev = require('electron-is-dev');
 
 const { menuTemplate } = require('./utils/menuTemplate');
-// const { registerHotkeys, unregisterHotkeysOnClose } = require('./hotkeys');
 const { createWindows, setWidthAndHeight } = require('./utils/windows');
-
 const { setIPCListeners } = require('./utils/IPC');
 const { closeSimulator } = require('./launch/closeSimulator');
-
-const { SerialPortObject } = require('./serial/nmea');
+const { initGlobals } = require('./utils/globals');
 
 let controlWindow;
 let videoWindow;
 
-// Global settings for TCP port and IP address, the "start serial port"-file, and the boat serial port and baud rate
-global.settings = {
-  port: 5000,
-  host: '127.0.0.1',
-  serialFile:
-    'C:/_work/FhSim/sfhdev/FhSimPlayPen_vs14_amd64/bin/aquaculturerobotics/runrtvisrunROV_ILOS_1.bat',
-  messageProtocol: 'OLD',
-  boatSerialPort: 'COM2',
-  boatSerialBaudRate: 256000,
-  boatSerialPortObject: null,
-};
-
-// Initialize a serial object with the port and baud rate given in the global settings
-global.settings.boatSerialPortObject = new SerialPortObject(
-  global.settings.boatSerialPort,
-  global.settings.boatSerialBaudRate,
-);
-
-// Global state objects
-global.toROV = {
-  surge: 0.0,
-  sway: 0.0,
-  heave: 0.0,
-  roll: 0.0,
-  pitch: 0.0,
-  yaw: 0.0,
-  autodepth: false,
-  autoheading: false,
-};
-global.fromROV = {
-  north: 0.0,
-  east: 0.0,
-  down: 0.0,
-  roll: 0.0,
-  pitch: 0.0,
-  yaw: 0.0,
-};
-global.bias = {
-  surge: 0.0,
-  sway: 0.0,
-  heave: 0.0,
-};
-
-/**
- * Global mode variable
- * 0 - Manual
- * 1 - DP mode
- * 2 - NF mode
- */
-global.mode = {
-  currentMode: 0,
-  nfAvailable: true,
-  dpAvailable: true,
-};
-
-/**
- * In IMC, positive and negative values of velocity is also indicating port/starboard direction
- * Positive velocity values: Starboard / styrbord
- * Negative velocity values: Port / babord
- * Direction property is therefore not needed
- */
-global.netfollowing = {
-  distance: 0,
-  velocity: 0,
-  degree: 0,
-  depth: 0,
-};
-
-/**
- * Add correct settings later
- * Placeholders for X, Y, Z for now
- */
-global.dynamicpositioning = {
-  latitude: 0,
-  longitude: 0,
-  heading: 0,
-  depth: 0,
-};
-
-global.boatposition = {
-  latitude: 0,
-  longitude: 0,
-};
+initGlobals();
 
 // Functions that are run when the app is ready
 app.on('ready', () => {
---
+++ public/utils/globals.js
@@ -0,0 +1,90 @@
+const { SerialPortObject } = require('../serial/nmea');
+
+const initGlobals = () => {
+  // Global settings for TCP port and IP adress, as well as the "start serial port"-file
+  global.settings = {
+    port: 5000,
+    host: '127.0.0.1',
+    serialFile:
+      'C:/_work/FhSim/sfhdev/FhSimPlayPen_vs14_amd64/bin/aquaculturerobotics/runrtvisrunROV_ILOS_1.bat',
+    messageProtocol: 'OLD',
+    boatSerialPort: 'COM2',
+    boatSerialBaudRate: 256000,
+    boatSerialPortObject: null,
+  };
+
+  // Initialize a serial object with the port and baud rate given in the global settings
+  global.settings.boatSerialPortObject = new SerialPortObject(
+    global.settings.boatSerialPort,
+    global.settings.boatSerialBaudRate,
+  );
+
+  // Global state objects
+  global.toROV = {
+    surge: 0.0,
+    sway: 0.0,
+    heave: 0.0,
+    roll: 0.0,
+    pitch: 0.0,
+    yaw: 0.0,
+    autodepth: false,
+    autoheading: false,
+  };
+  global.fromROV = {
+    north: 0.0,
+    east: 0.0,
+    down: 0.0,
+    roll: 0.0,
+    pitch: 0.0,
+    yaw: 0.0,
+  };
+  global.bias = {
+    surge: 0.0,
+    sway: 0.0,
+    heave: 0.0,
+  };
+
+  /**
+   * Global mode variable
+   * 0 - Manual
+   * 1 - DP mode
+   * 2 - NF mode
+   */
+  global.mode = {
+    currentMode: 0,
+    nfAvailable: true,
+    dpAvailable: true,
+  };
+
+  /**
+   * In IMC, positive and negative values of velocity is also indicating port/starboard direction
+   * Positive velocity values: Starboard / styrbord
+   * Negative velocity values: Port / babord
+   * Direction property is therefore not needed
+   */
+  global.netfollowing = {
+    distance: 0,
+    velocity: 0,
+    degree: 0,
+    depth: 0,
+  };
+
+  /**
+   * Add correct settings later
+   * Placeholders for X, Y, Z for now
+   */
+  global.dynamicpositioning = {
+    latitude: 0,
+    longitude: 0,
+    heading: 0,
+    depth: 0,
+  };
+
+  // Boat coordinates
+  global.boatposition = {
+    latitude: 0,
+    longitude: 0,
+  };
+};
+
+module.exports = { initGlobals };
