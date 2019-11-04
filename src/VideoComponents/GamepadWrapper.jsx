c1be24caeebf75e81d0ba7f599a01dd99d6fcdd8
Tora Seim Gunstad <toragunstad@gmail.com>
2019-11-04 09:33:10 +0100

    feat: popup notifications when xbox (dis)/connected

--- src/VideoComponents/GamepadWrapper.jsx
+++ src/VideoComponents/GamepadWrapper.jsx
@@ -1,66 +1,54 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { useAlert } from 'react-alert';
 import Gamepad from 'react-gamepad';
 
-const { alert } = useAlert();
+function GamepadWrapper() {
+  const alert = useAlert();
 
-class GamepadWrapper extends Component {
-  connectHandler(gamepadIndex) {
+  const connectHandler = gamepadIndex => {
     console.log(`Gamepad ${gamepadIndex} connected!`);
-  }
+    alert.success('XBox Controller is connected!');
+  };
 
-  disconnectHandler(gamepadIndex) {
+  const disconnectHandler = gamepadIndex => {
     console.log(`Gamepad ${gamepadIndex} disconnected!`);
-  }
+    alert.error('Note! XBox Controller disconnected');
+  };
 
-  buttonChangeHandler(button, down) {
+  const buttonChangeHandler = (button, down) => {
     if (down) {
       // window.ipcRenderer is fetched from preload.js
       window.ipcRenderer.send('button-click', { button, value: 1.0 });
     }
-  }
+  };
 
-  axisChangeHandler(button, value) {
+  const axisChangeHandler = (button, value) => {
     window.ipcRenderer.send('button-click', { button, value });
-  }
+  };
 
-  buttonUpHandler(button) {
+  const buttonUpHandler = button => {
     window.ipcRenderer.send('button-up-down', { button, down: false });
-  }
+  };
 
-  buttonDownHandler(button) {
+  const buttonDownHandler = button => {
     window.ipcRenderer.send('button-up-down', { button, down: true });
-  }
+  };
 
-  // Tanken er at man lager en ekstra funksjon/CustomHook til å kalle på de to andre, og bruke denne ved onConnect
-  // Feilmelding om at "connectHandler is not defined"?
-  useConnection(gamepadIndex) {
-    connectHandler(gamepadIndex);
-    alert.success('XBox Controller is connected!');
-  }
-
-  useDisconnection(gamepadIndex) {
-    disconnectHandler(gamepadIndex);
-    alert.error('Note! XBox Controller disconnected');
-  }
-
-  render() {
-    return (
-      <div>
-        <Gamepad
-          onConnect={this.useConnection}
-          onDisconnect={this.useDisconnection}
-          onButtonChange={this.buttonChangeHandler}
-          onAxisChange={this.axisChangeHandler}
-          onButtonDown={this.buttonDownHandler}
-          onButtonUp={this.buttonUpHandler}
-          deadZone={0.3}
-        >
-          <span></span>
-        </Gamepad>
-      </div>
-    );
-  }
+  return (
+    <div>
+      <Gamepad
+        onConnect={connectHandler}
+        onDisconnect={disconnectHandler}
+        onButtonChange={buttonChangeHandler}
+        onAxisChange={axisChangeHandler}
+        onButtonDown={buttonDownHandler}
+        onButtonUp={buttonUpHandler}
+        deadZone={0.3}
+      >
+        <span></span>
+      </Gamepad>
+    </div>
+  );
 }
 
 export default GamepadWrapper;
--- src/index.jsx
+++ src/index.jsx
@@ -8,11 +8,13 @@ import AlertTemplate from 'react-alert-template-basic';
 import ViewManager from './ViewManager';
 import './index.css';
 
+// place pop-up messages in bottom center of screen for 5s
 const options = {
   timeout: 5000,
   position: positions.BOTTOM_CENTER,
 };
 
+// wrap our application with the Alert-Provider
 const App = () => (
   <Provider template={AlertTemplate} {...options}>
     <ViewManager />
