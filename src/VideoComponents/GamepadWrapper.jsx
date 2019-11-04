6cd57920518a4bd577b851d76eab2038299b39d1
Tora Seim Gunstad <toragunstad@gmail.com>
2019-11-04 12:24:48 +0100

    Feature/xbox connected (#198)
    * feat: add popup when xbox connected - NF
    
    * feat: popup notifications when xbox (dis)/connected

--- src/VideoComponents/GamepadWrapper.jsx
+++ src/VideoComponents/GamepadWrapper.jsx
@@ -1,51 +1,54 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { useAlert } from 'react-alert';
 import Gamepad from 'react-gamepad';
 
-class GamepadWrapper extends Component {
-  connectHandler(gamepadIndex) {
+function GamepadWrapper() {
+  const alert = useAlert();
+
+  const connectHandler = gamepadIndex => {
     console.log(`Gamepad ${gamepadIndex} connected!`);
-  }
+    alert.success('XBox Controller is connected!');
+  };
 
-  disconnectHandler(gamepadIndex) {
+  const disconnectHandler = gamepadIndex => {
     console.log(`Gamepad ${gamepadIndex} disconnected!`);
-  }
+    alert.error('Note! XBox Controller disconnected');
+  };
 
-  buttonChangeHandler(button, down) {
+  const buttonChangeHandler = (button, down) => {
     if (down) {
       // window.ipcRenderer is fetched from preload.js
       window.ipcRenderer.send('button-click', { button, value: 1.0 });
     }
-  }
+  };
 
-  axisChangeHandler(button, value) {
+  const axisChangeHandler = (button, value) => {
     window.ipcRenderer.send('button-click', { button, value });
-  }
+  };
 
-  buttonUpHandler(button) {
+  const buttonUpHandler = button => {
     window.ipcRenderer.send('button-up-down', { button, down: false });
-  }
+  };
 
-  buttonDownHandler(button) {
+  const buttonDownHandler = button => {
     window.ipcRenderer.send('button-up-down', { button, down: true });
-  }
+  };
 
-  render() {
-    return (
-      <div>
-        <Gamepad
-          onConnect={this.connectHandler}
-          onDisconnect={this.disconnectHandler}
-          onButtonChange={this.buttonChangeHandler}
-          onAxisChange={this.axisChangeHandler}
-          onButtonDown={this.buttonDownHandler}
-          onButtonUp={this.buttonUpHandler}
-          deadZone={0.3}
-        >
-          <span></span>
-        </Gamepad>
-      </div>
-    );
-  }
+  return (
+    <div>
+      <Gamepad
+        onConnect={connectHandler}
+        onDisconnect={disconnectHandler}
+        onButtonChange={buttonChangeHandler}
+        onAxisChange={axisChangeHandler}
+        onButtonDown={buttonDownHandler}
+        onButtonUp={buttonUpHandler}
+        deadZone={0.3}
+      >
+        <span></span>
+      </Gamepad>
+    </div>
+  );
 }
 
 export default GamepadWrapper;
--- src/index.jsx
+++ src/index.jsx
@@ -3,7 +3,22 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { positions, Provider } from 'react-alert';
+import AlertTemplate from 'react-alert-template-basic';
 import ViewManager from './ViewManager';
 import './index.css';
 
-ReactDOM.render(<ViewManager />, document.getElementById('root'));
+// place pop-up messages in bottom center of screen for 5s
+const options = {
+  timeout: 5000,
+  position: positions.BOTTOM_CENTER,
+};
+
+// wrap our application with the Alert-Provider
+const App = () => (
+  <Provider template={AlertTemplate} {...options}>
+    <ViewManager />
+  </Provider>
+);
+
+ReactDOM.render(<App />, document.getElementById('root'));
